2a50e721b703f824324c79875d14bbacb22f8e0f
TakanariShimbo <takanari.shimbo@gmail.com>
2024-02-11 00:20:07 +0900

    update

--- bayesian_network/chi.py
+++ bayesian_network/chi.py
@@ -7,20 +7,14 @@ from scipy import stats
 def chi_square_test(col1: str, col2: str, cond_cols: List[str], bin_df: pd.DataFrame) -> float:
     # Step 1: Check if the arguments are valid and type conversions.
     if col1 in cond_cols:
-        raise ValueError(
-            f"The variables col1 or col2 can't be in cond_cols. Found {col1} in cond_cols."
-        )
+        raise ValueError(f"The variables col1 or col2 can't be in cond_cols. Found {col1} in cond_cols.")
     if col2 in cond_cols:
-        raise ValueError(
-            f"The variables col1 or col2 can't be in cond_cols. Found {col2} in cond_cols."
-        )
+        raise ValueError(f"The variables col1 or col2 can't be in cond_cols. Found {col2} in cond_cols.")
 
     # Step 2: Do contingency test.
     if len(cond_cols) == 0:
         # Do a simple contingency test if there are no conditional variables.
-        chi, p_value, dof, _ = stats.chi2_contingency(
-            bin_df.groupby([col1, col2]).size().unstack(col2, fill_value=0), lambda_="pearson"
-        )
+        chi, p_value, dof, _ = stats.chi2_contingency(bin_df.groupby([col1, col2]).size().unstack(col2, fill_value=0), lambda_="pearson")
 
     else:
         # If there are conditionals variables, iterate over unique states and do the contingency test.
@@ -28,9 +22,7 @@ def chi_square_test(col1: str, col2: str, cond_cols: List[str], bin_df: pd.DataF
         dof = 0
         for parsed_cond, parsed_bin_df in bin_df.groupby(cond_cols):
             try:
-                chi_i, _, dof_i, _ = stats.chi2_contingency(
-                    parsed_bin_df.groupby([col1, col2]).size().unstack(col2, fill_value=0), lambda_="pearson"
-                )
+                chi_i, _, dof_i, _ = stats.chi2_contingency(parsed_bin_df.groupby([col1, col2]).size().unstack(col2, fill_value=0), lambda_="pearson")
                 chi += chi_i
                 dof += dof_i
             except ValueError:
@@ -48,6 +40,4 @@ def chi_square_test(col1: str, col2: str, cond_cols: List[str], bin_df: pd.DataF
         p_value = 1 - stats.chi2.cdf(chi, df=dof)
 
     # Step 3: Return the values
-    return p_value   # type: ignore
-
-
+    return p_value  # type: ignore
