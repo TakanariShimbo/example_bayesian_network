cdcc69187e6d654cadf967e26b869ebaabc37650
shinbot <shinbot@uniontool.co.jp>
2024-02-13 15:37:29 +0900

    update

--- bayesian_network/main.py
+++ bayesian_network/main.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Dict
 
 import pandas as pd
 
@@ -10,26 +10,8 @@ from .network_visualizer import NetworkVisualizer
 
 
 class BayesianNetwork:
-    def __init__(self, bin_df: pd.DataFrame, n_dim_total: int = 2):
-        self._n_dim_total = n_dim_total
-        self._bin_df = bin_df
-        self._connection_dfs: List[pd.DataFrame] = []
-        self._labels: List[str] = []
-
-        column_names = bin_df.columns.to_list()
-        init_connection_df = self._get_init_connection_df(column_names=column_names)
-        self._connection_dfs.append(init_connection_df)
-        self._labels.append("Init")
-
-    @property
-    def connection_df_dict(self):
-        connection_df_dict_ = {}
-        for label, connection_df in zip(self._labels, self._connection_dfs):
-            connection_df_dict_[label] = connection_df
-        return connection_df_dict_
-
     @staticmethod
-    def _get_init_connection_df(column_names: List[str]):
+    def _get_init_connection_df(column_names: List[str]) -> pd.DataFrame:
         init_connection_df = pd.DataFrame(data=False, index=column_names, columns=column_names, dtype=bool)
 
         for i, column1 in enumerate(column_names[:-1]):
@@ -40,7 +22,7 @@ class BayesianNetwork:
         return init_connection_df
 
     @staticmethod
-    def _analyze_connection_step(n_dim: int, connection_checker: ConnectionChecker, connection_df: pd.DataFrame):
+    def _analyze_connection_step(n_dim: int, connection_checker: ConnectionChecker, connection_df: pd.DataFrame) -> pd.DataFrame:
         connection_infos_collector = ConnectionInfosCollector(n_dim=n_dim, connection_df=connection_df, connection_checker=connection_checker)
         print(f"----------------------------------------------------")
         print(f"              Collect ConnectionInfos               ")
@@ -61,31 +43,39 @@ class BayesianNetwork:
 
         return new_connect_df
 
-    def analyze_connection(self, p_threshold: float = 0.05):
-        connection_checker = ConnectionChecker(bin_df=self._bin_df, p_threshold=p_threshold)
+    @classmethod
+    def analyze_connection(cls, bin_df: pd.DataFrame, n_dim_total: int = 2, p_threshold: float = 0.05) -> Dict[str, pd.DataFrame]:
+        connection_checker = ConnectionChecker(bin_df=bin_df, p_threshold=p_threshold)
 
-        for n_dim in range(self._n_dim_total + 1):
+        connection_dfs: List[pd.DataFrame] = []
+        labels: List[str] = []
+
+        column_names = bin_df.columns.to_list()
+        init_connection_df = cls._get_init_connection_df(column_names=column_names)
+        connection_dfs.append(init_connection_df)
+        labels.append("Init")
+
+        for n_dim in range(n_dim_total+1):
             print(f"====================================================")
             print(f"                     DIM{n_dim}                     ")
             print(f"====================================================")
-            new_connection_df = self._analyze_connection_step(n_dim=n_dim, connection_checker=connection_checker, connection_df=self._connection_dfs[-1])
-            self._connection_dfs.append(new_connection_df)
-            self._labels.append(f"Level{n_dim}")
+            new_connection_df = cls._analyze_connection_step(n_dim=n_dim, connection_checker=connection_checker, connection_df=connection_dfs[-1])
+            connection_dfs.append(new_connection_df)
+            labels.append(f"Level{n_dim}")
             print()
 
-        return self.connection_df_dict
+        connection_df_dict = {}
+        for label, connection_df in zip(labels, connection_dfs):
+            connection_df_dict[label] = connection_df
+        return connection_df_dict
 
-    def analyze_closeness(self, target_col: str):
-        closeness_dfs = []
-        for connection_df in self._connection_dfs:
+    @staticmethod
+    def analyze_closeness(target_col: str, connection_df_dict: Dict[str, pd.DataFrame]) -> Dict[str, pd.DataFrame]:
+        closeness_df_dict = {}
+        for label, connection_df in connection_df_dict.items():
             analyzer = ClosenessAnalyzer(connection_df=connection_df)
             closeness_df = analyzer.analyze(target_column=target_col)
-            closeness_dfs.append( closeness_df )
-
-        closeness_df_dict = {}
-        for label, closeness_df in zip(self._labels, closeness_dfs):
             closeness_df_dict[label] = closeness_df
-
         return closeness_df_dict
 
     @staticmethod
