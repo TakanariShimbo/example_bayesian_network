0b3870f57a2478fb826fe5d6c79d9025ec7efbd7
shinbot <shinbot@uniontool.co.jp>
2024-02-22 08:50:33 +0900

    update

--- bayesian_network/connection_infos_applyer.py
+++ bayesian_network/connection_infos_applyer.py
@@ -15,14 +15,15 @@ class ConnectionInfosApplyer:
         self._connection_infos = connection_infos
 
     @staticmethod
-    def _generate_xxxxxs(n_dim: int):
-        permutations = list(itertools.permutations(np.arange(n_dim) + 2))
-        xxxxxs = []
-        for xxxxx_list in permutations:
-            xxx = ''.join(map(str, xxxxx_list))
+    def _generate_xxxxxs_list(n_dim: int) -> List[List[str]]:
+        base_xxx_list = np.arange(n_dim) + 2
+        permutations_xxx_list = list(itertools.permutations(base_xxx_list))
+        xxxxxs_list = []
+        for xxx_list in permutations_xxx_list:
+            xxx = ''.join(map(str, xxx_list))
             xxxxx = f"0{xxx}1"
-            xxxxxs.append(xxxxx)
-        return xxxxxs
+            xxxxxs_list.append([xxxxx])
+        return xxxxxs_list
 
     @staticmethod
     def _check_ab_is_connecting(index_pair: List[int], columns: List[str], connection_df: pd.DataFrame) -> bool:
@@ -34,31 +35,26 @@ class ConnectionInfosApplyer:
         return is_ab_connecting  # type: ignore
 
     @staticmethod
-    def _convert_xxxxx_to_index_pairs(xxxxx: str) -> List[List[int]]:
-        return [
-            [
-                int(xxxxx[i]),
-                int(xxxxx[i + 1]),
-            ]
-            for i in range(len(xxxxx) - 1)
-        ]
+    def _convert_xxxxx_to_list(xxxxx: str) -> List[int]:
+        return [int(xxxxx[i]) for i in range(len(xxxxx) - 1)]
 
-    def _check_xxxxx_is_connecting(self, xxxxx: str, columns: List[str]) -> bool:
-        index_pairs = self._convert_xxxxx_to_index_pairs(xxxxx=xxxxx)
-
-        is_xxxxx_connecting = True
-        for index_pair in index_pairs:
-            is_ab_connecting = self._check_ab_is_connecting(index_pair=index_pair, columns=columns, connection_df=self._connection_df)
-            is_xxxxx_connecting = is_xxxxx_connecting and is_ab_connecting
-            if not is_xxxxx_connecting:
-                break
-        return is_xxxxx_connecting
-
-    def _check_xxxxxs_is_connecting(self, columns: List[str]) -> bool:
-        xxxxxs = self._generate_xxxxxs(n_dim=self._n_dim)
+    def _check_xxxxxs_is_connecting(self, xxxxxs: List[str], columns: List[str]) -> bool:
+        is_xxxxxs_connecting = True
         for xxxxx in xxxxxs:
-            is_xxxxx_connecting = self._check_xxxxx_is_connecting(xxxxx=xxxxx, columns=columns)
-            if is_xxxxx_connecting:
+            xxxxx_list = self._convert_xxxxx_to_list(xxxxx=xxxxx)
+            for i in range(len(xxxxx_list)-1):
+                index_pair = [xxxxx_list[i], xxxxx_list[i+1]]
+                is_ab_connecting = self._check_ab_is_connecting(index_pair=index_pair, columns=columns, connection_df=self._connection_df)
+                is_xxxxxs_connecting = is_xxxxxs_connecting and is_ab_connecting
+                if not is_xxxxxs_connecting:
+                    break
+        return is_xxxxxs_connecting
+
+    def _check_xxxxxs_list_is_connecting(self, columns: List[str]) -> bool:
+        xxxxxs_list = self._generate_xxxxxs_list(n_dim=self._n_dim)
+        for xxxxxs in xxxxxs_list:
+            is_xxxxxs_connecting = self._check_xxxxxs_is_connecting(xxxxxs=xxxxxs, columns=columns)
+            if is_xxxxxs_connecting:
                 return True
         return False
 
@@ -73,7 +69,7 @@ class ConnectionInfosApplyer:
         applicable_disconnection_info = []
         for disconnection_info in disconnection_infos:
             columns = [disconnection_info.column1, disconnection_info.column2, *disconnection_info.condition_columns]
-            if not self._check_xxxxxs_is_connecting(columns=columns):
+            if not self._check_xxxxxs_list_is_connecting(columns=columns):
                 continue
 
             print(disconnection_info)
@@ -84,7 +80,7 @@ class ConnectionInfosApplyer:
     def apply(self):
         for disconnection_info in self._connection_infos:
             columns = [disconnection_info.column1, disconnection_info.column2, *disconnection_info.condition_columns]
-            if not self._check_xxxxxs_is_connecting(columns=columns):
+            if not self._check_xxxxxs_list_is_connecting(columns=columns):
                 continue
 
             print(disconnection_info)
